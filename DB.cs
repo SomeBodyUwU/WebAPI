18e358b4b99a3dc2b1cf8d454a391c83224a15a0
SomeBodyUwU <127203403+SomeBodyUwU@users.noreply.github.com>
2024-09-16 17:16:59 +0300

    Update DB.cs

--- DB.cs
+++ DB.cs
@@ -4,7 +4,7 @@ namespace API;
 
 public class DB
 {
-    private MySqlConnection _connection = new MySqlConnection("server=mysql-20230529230403.mysql.database.azure.com;port=3306;username=Dmytro@mysql-20230529230403;database=topanime_schema;password=RHJrj500;Allow User Variables=true");
+    private MySqlConnection _connection = new MySqlConnection("server=mysql-20230529230403.mysql.database.azure.com;port=3306;username=Dmytro@mysql-20230529230403;database=topanime_schema;password=//password;Allow User Variables=true");
 
     public MySqlConnection GetConnection()
     {
